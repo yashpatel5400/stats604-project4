3e395e77f15dfd24dad362ed3acf2f4c6f4f3007
Yash Patel <ypatel5400@gmail.com>
2022-11-02 18:19:29 -0400

    Add make and main boilerplate

---
+++ Makefile
@@ -0,0 +1,10 @@
+make clean:
+	rm -rf raw_data
+
+make:
+
+make predictions:
+
+make rawdata:
+
+make report:
---
+++ predictor/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+"""Main entrypoint for weather predictions"""
+
+__author__ = "Vinod Raman, Unique Subedi, Seamus Somerstep, and Yash Patel"
+__license__ = "MIT"
+__version__ = "1.0"
+__status__ = "Dev"
+
+if __name__ == '__main__':
+    pass

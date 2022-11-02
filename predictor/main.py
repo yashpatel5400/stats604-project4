bda959484474d8816969033c8bb9968ceeec3808
Yash Patel <ypatel5400@gmail.com>
2022-11-02 19:02:34 -0400

    Temp result

--- predictor/main.py
+++ predictor/main.py
@@ -6,5 +6,41 @@ __license__ = "MIT"
 __version__ = "1.0"
 __status__ = "Dev"
 
+import pandas as pd
+import datetime
+
+def format_output(date_to_prediction):
+    """Takes a dictionary that maps dates to dataframes with the weather stations as 
+    rows and min, avg, max as columns and spits back the formatted string desired as output
+    """
+    start_date = list(date_to_prediction.keys())[0]
+    return f"{start_date}, {', '.join([', '.join(date_to_prediction[date].values.flatten().astype(str)) for date in date_to_prediction])}"
+    
 if __name__ == '__main__':
-    pass
+    stations = [
+        "PANC",
+        "KBOI",
+        "KORD",
+        "KDEN",
+        "KDTW",
+        "PHNL",
+        "KIAH",
+        "KMIA",
+        "KMIC",
+        "KOKC",
+        "KBNA",
+        "KJFK",
+        "KPHX",
+        "KPWM",
+        "KPDX",
+        "KSLC",
+        "KSAN",
+        "KSFO",
+        "KSEA",
+        "KDCA",
+    ]
+    start_date = datetime.datetime.strptime("10/10/11", "%m/%d/%y")
+    dates = [start_date + datetime.timedelta(days=1 + delta) for delta in range(5)]
+    predictions = {date: pd.DataFrame(0, columns = ["Min", "Avg", "Max"], index = stations) for date in dates}
+    output = format_output(predictions)
+    print(output)

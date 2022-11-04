33f1f9762f2efa5eebf687621206eb4a0904f2e7
Yash Patel <ypatel5400@gmail.com>
2022-11-04 18:18:28 -0400

    Bit of refactor on wunderground

--- raw_data/wunderground_download.py
+++ raw_data/wunderground_download.py
@@ -8,19 +8,21 @@ import os
 
 from predictor.utils import stations
 
-headers = {
-    'sec-ch-ua': '"Chromium";v="106", "Google Chrome";v="106", "Not;A=Brand";v="99"',
-    'Accept': 'application/json, text/plain, */*',
-    'Referer': 'https://www.wunderground.com/',
-    'sec-ch-ua-mobile': '?0',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
-    'sec-ch-ua-platform': '"macOS"',
-}
+def download_wunderground(end_date_str="11/03/22", download_window=5):
+    """Downloads data from Wunderground from end_date-download_window to end_date. Note that
+    using too large a download_window (i.e. > 20) will cause this to error out
+    """
 
-for station in stations:
-    download_window = 5 # download today and the previous (download_window-1) days, for a total of download_window days
-    
-    end_date = datetime.datetime.strptime("11/03/22", "%m/%d/%y") # can also use datetime.today() for today's date
+    headers = {
+        'sec-ch-ua': '"Chromium";v="106", "Google Chrome";v="106", "Not;A=Brand";v="99"',
+        'Accept': 'application/json, text/plain, */*',
+        'Referer': 'https://www.wunderground.com/',
+        'sec-ch-ua-mobile': '?0',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
+        'sec-ch-ua-platform': '"macOS"',
+    }
+
+    end_date = datetime.datetime.strptime(end_date_str, "%m/%d/%y") # can also use datetime.today() for today's date
     start_date = end_date - datetime.timedelta(days=(download_window-1))
     start_date_str = f"{start_date:%Y%m%d}"
     end_date_str = f"{end_date:%Y%m%d}"
@@ -32,8 +34,11 @@ for station in stations:
         'endDate': end_date_str,
     }
     response = requests.get(f'https://api.weather.com/v1/location/{station}:9:US/observations/historical.json', params=params, headers=headers)
-    data = json.loads(response.text)["observations"]
-    
-    os.makedirs("wunderground", exist_ok=True)
-    with open(os.path.join("wunderground", f"{station}.json"), "w") as f:
-        json.dump(data, f)
+    return json.loads(response.text)["observations"]
+
+if __name__ == "__main__":
+    for station in stations:
+        data = download_wunderground(end_date_str=datetime.today(), download_window=5)
+        os.makedirs("wunderground", exist_ok=True)
+        with open(os.path.join("wunderground", f"{station}.json"), "w") as f:
+            json.dump(data, f)

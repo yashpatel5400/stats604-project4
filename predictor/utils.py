5d24065f85a57a7ff24790a64033f89b8ab1b4ce
Yash Patel <ypatel5400@gmail.com>
2022-11-04 17:57:13 -0400

    Small typos

--- predictor/utils.py
+++ predictor/utils.py
@@ -31,6 +31,6 @@ def load_data(station=None):
     station_to_noaa_data = {}
     station_to_wunderground_data = {}
     for station in stations:
-        station_to_noaa_data[station] = pd.from_csv(os.path.join("..", "noaa", f"{station}.csv"))
-        station_to_wunderground_data[station] = pd.from_csv(os.path.join("..", "wunderground", f"{station}.csv"))
+        station_to_noaa_data[station] = pd.read_csv(os.path.join("..", "data", "noaa", f"{station}.csv"))
+        station_to_wunderground_data[station] = pd.read_csv(os.path.join("..", "data", "wunderground", f"{station}.csv"))
     return station_to_noaa_data, station_to_wunderground_data

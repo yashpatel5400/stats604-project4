c1fdc46091042f299a051ac086c17ea69ad60e7e
Vinod Raman <vkraman@umich.edu>
2022-11-05 13:20:48 -0400

    removed a print statement

--- predictor/vinod.py
+++ predictor/vinod.py
@@ -29,7 +29,6 @@ if __name__ == '__main__':
         df = pd.read_csv('data/wunderground/' + station + '.csv')
         df['date'] = pd.to_datetime(df['date']).dt.date
         temp_max = df.groupby(['date'], sort=False)['temp'].max()
-        print(temp_max)
         temp_min = df.groupby(['date'], sort=False)['temp'].min()
         temp_mean = df.groupby(['date'], sort=False)['temp'].mean()
         stations_data.append([temp_min[-1], round(temp_mean[-1], 1), temp_max[-1]])

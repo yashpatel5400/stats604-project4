01a92ce4ebc9470c63fcc5c6d01287bd929cc75f
Vinod Raman <vkraman@umich.edu>
2022-11-06 18:23:00 -0500

    testing for november

--- predictor/eval.py
+++ predictor/eval.py
@@ -104,7 +104,7 @@ def eval(start_eval_date, eval_len, model):
     return mses
 
 if __name__ == "__main__":
-    start_eval_str = "2021-10-01" # when eval period starts (must follow %Y-%m-%d format)
+    start_eval_str = "2021-11-01" # when eval period starts (must follow %Y-%m-%d format)
     start_eval_date = datetime.datetime.strptime(start_eval_str, "%Y-%m-%d") 
     eval_len = 10 # how many days we running evaluation for
 

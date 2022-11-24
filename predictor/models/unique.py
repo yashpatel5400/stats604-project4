a6799a6e25f4e56f5dc8430c83e417b153a0c700
Unique Subedi <subedi@stat-subedi-mba.lan>
2022-11-24 11:28:23 -0500

    MLPRegressor

--- predictor/models/unique.py
+++ predictor/models/unique.py
@@ -5,10 +5,13 @@ import numpy as np
 import sys
 from matplotlib import pyplot as plt
 import utils
+import time
 from statsmodels.tsa.arima.model import ARIMA
 from statsmodels.tsa.statespace.sarimax import SARIMAX
 from predictor.models.predictor_scaffold import Predictor
-
+from sklearn.multioutput import MultiOutputRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.neural_network import MLPRegressor
 
 
 
@@ -84,3 +87,28 @@ class ArimaPredictor(Predictor):
         stations_data = np.array(stations_data).flatten()
         return stations_data
 
+def create_regression_data(data, window_size):
+    X, y = [], []
+    target_data = data[["temp_min","temp_mean","temp_max"]].values
+    prediction_window = 5
+    for i in range(len(data) - (window_size + prediction_window + 1)):
+        X.append(data.values[i:i+window_size,:-1].flatten())
+        y.append(target_data[i+window_size+1:i+window_size+1+prediction_window].flatten())
+    test_X = data.values[-window_size-1:-1,:-1].flatten().reshape(1, -1) # the final frame used for future prediction
+    return np.array(X), np.array(y), test_X
+
+class NLinPredict(Predictor):
+    def __init__(self):
+        pass
+
+    def predict(self, data):
+        stations_data = []
+        start = time.time()
+        for station in utils.stations:
+            window_size = 3
+            X, y, test_X = create_regression_data(data[station]["wunderground"], window_size) 
+            reg = MLPRegressor(random_state=1, hidden_layer_sizes=(20,20, ), max_iter=2000, solver='lbfgs', learning_rate= 'adaptive').fit(X, y)
+            stations_data.append(reg.predict(test_X))
+        end = time.time()
+        print(f"Performed prediction in: {end - start} s")
+        return np.array(stations_data).flatten()

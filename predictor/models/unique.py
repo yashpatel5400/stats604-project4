f008a70cbc0282be8ad4688f9c84e04d8ac33c03
Unique Subedi <subedi@stat-subedi-mba.lan>
2022-11-24 00:25:39 -0500

    arima with seasonal

--- predictor/models/unique.py
+++ predictor/models/unique.py
@@ -6,6 +6,7 @@ import sys
 from matplotlib import pyplot as plt
 import utils
 from statsmodels.tsa.arima.model import ARIMA
+from statsmodels.tsa.statespace.sarimax import SARIMAX
 from predictor.models.predictor_scaffold import Predictor
 
 
@@ -29,7 +30,10 @@ class HistoricAveragePredictor(Predictor):
             for i in range(1, 6):
                 date_ = (current_date + pd.DateOffset(days=i))
                 stations_data.append(df[df.index == (date_.month, date_.day)].values.flatten())
+           
+       
              
+           
         stations_data = np.array(stations_data).flatten()
         return stations_data
 
@@ -43,30 +47,32 @@ class ArimaPredictor(Predictor):
 
         stations_data = []
         for station in utils.stations:
-            noaa = data[station]["noaa"]
-            noaa = noaa.loc[:, [ "TMIN", "TAVG", "TMAX"]]
-            noaa = noaa.iloc[-100:, :]
-            noaa = noaa.asfreq('D')
-            #noaa = noaa.dropna(axis =0)
-            noaa = noaa*0.18+32.0
+
+            df = data[station]["wunderground"]
+            temp_max = df.groupby(by = [df.index.date])['temp'].max().asfreq('D')
+            temp_min = df.groupby(by = [df.index.date])['temp'].min().asfreq('D')
+            temp_avg = df.groupby(by = [df.index.date])['temp'].mean().asfreq('D')
             
             
 
            
         
      
-            min_model = ARIMA(noaa.TMIN, order=(2,1,2)).fit()
-            #min_model.predict().plot(label = "prediction")
-            #noaa.TMIN.plot(label = "true")
-            #plt.legend()
-            #plt.show()
-            avg_model = ARIMA(noaa.TAVG, order=(2,1,2)).fit()
-            max_model = ARIMA(noaa.TMAX, order=(2,1,2)).fit()
+            min_model = SARIMAX(temp_min, order=(3,1,1), seasonal_order=(1, 0, 0, 12)).fit(disp=False)
+           
+    
+            avg_model = SARIMAX(temp_avg, order=(3,1,1), seasonal_order=(1, 0, 0, 12)).fit(disp=False)
+            #avg_model.predict(start = temp_avg.index[-100]).plot(label = "prediction")
+            #avg_model.forecast(steps = 5, alpha = 0.95).plot(label = "forecast")
+            # plt.legend()
+            # plt.title(str(station))
+            # plt.show()
+            max_model = SARIMAX(temp_max, order=(3,1,1), seasonal_order=(1, 0 , 0, 12)).fit(disp=False)
             
             min_fc = min_model.forecast(steps = 5)
             avg_fc = avg_model.forecast(steps = 5)
             max_fc  = max_model.forecast(steps = 5)
-
+            
         
             
             stations_data.append(np.vstack((min_fc.values, avg_fc.values, max_fc.values)).flatten())
@@ -77,5 +83,4 @@ class ArimaPredictor(Predictor):
            
         stations_data = np.array(stations_data).flatten()
         return stations_data
-
 
